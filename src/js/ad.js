b1299abe7c1ba56e9189fe1510c271d5d8698f90
hill <hillcom@qq.com>
2013-07-19 00:07:29 +0800

    Find the disable douban ad close function.

--- src/js/ad.js
+++ src/js/ad.js
@@ -3,24 +3,11 @@
 //
 define(function(require, exports, module) {
 
-    var $ = require('jquery');
-
-    function DoubanAd(elem) {
-        this.$elem = $(elem);
-    }
-
-    module.exports = DoubanAd;
-
-    DoubanAd.prototype = {
-
-        remove: function () {
-            this.$elem.remove();
-            return this;
-        },
-
-        hide: function () {
-            this.$elem.hide();
-            return this;
+    module.exports = {
+        disable: function () {
+            if (window.disableBannerAd) {
+                disableBannerAd();
+            }
         }
     };
 
