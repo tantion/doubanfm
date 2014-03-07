fe8d247bf4c09b68a64ab2072eb30005b215e0a9
tantion <hillcom@qq.com>
2014-03-08 01:48:15 +0800

    Skip video advertisement

--- src/js/ad-block.js
+++ src/js/ad-block.js
@@ -5,18 +5,38 @@
 define(function(require, exports, module) {
     "use strict";
 
+    var $ = require('jquery');
+
     function removeBannerAd () {
         window.initBannerAd = window.disableBannerAd = window.enableBannerAd = function () {};
     }
 
+    function autoSkipVideoAd () {
+        /* global DBR: true */
+        if (window.DBR && !DBR._play_video) {
+            DBR._play_video = DBR.play_video;
+            DBR.play_video = function () {
+                setTimeout(function () {
+                    DBR.swf().video_complete(100);
+                }, 100);
+            };
+        }
+    }
+
     module.exports = {
         block: function () {
-            removeBannerAd();
-
             if (location.hostname === 'douban.fm' && location.pathname === '/') {
+                // Disabled Banner Ad
+                removeBannerAd();
                 Do.ready('fm-bannerad', 'fm-bgad', function() {
                     window.disableBannerAd();
                     removeBannerAd();
+                });
+
+                // Skip Video Ad
+                autoSkipVideoAd();
+                Do.ready('fm-player', function () {
+                    autoSkipVideoAd();
                 });
             }
         }
