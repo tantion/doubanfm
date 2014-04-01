e6d3d64a219d3ba8c1e16b76f62ff9cfef708bf7
tantion <hillcom@qq.com>
2014-04-02 03:59:59 +0800

    improve programme download

--- src/angular-app.js
+++ src/angular-app.js
@@ -1,4 +1,4 @@
-/*! douban-fm-improve - v1.8.0 - 2014-04-01
+/*! douban-fm-improve - v1.8.0 - 2014-04-02
 * https://github.com/tantion/doubanfm
 * Copyright (c) 2014 tantion; Licensed MIT */
 angular.module("ui.bootstrap", ["ui.bootstrap.tpls", "ui.bootstrap.transition","ui.bootstrap.collapse","ui.bootstrap.accordion","ui.bootstrap.alert","ui.bootstrap.bindHtml","ui.bootstrap.buttons","ui.bootstrap.carousel","ui.bootstrap.position","ui.bootstrap.datepicker","ui.bootstrap.dropdownToggle","ui.bootstrap.modal","ui.bootstrap.pagination","ui.bootstrap.tooltip","ui.bootstrap.popover","ui.bootstrap.progressbar","ui.bootstrap.rating","ui.bootstrap.tabs","ui.bootstrap.timepicker","ui.bootstrap.typeahead"]);
@@ -4121,6 +4121,158 @@ angular
 
 angular
 .module('fmApp')
+.controller('SearchController', ['$scope', '$location', 'baidu', 'download', '$modal', '_', 'async', '$timeout',
+    function ($scope, $location, baidu, download, $modal, _, async, $timeout) {
+    "use strict";
+
+    var params = $location.search();
+
+    $scope.query = params.query;
+
+    $scope.alert = {};
+    $scope.data = {};
+    $scope.status = {};
+    $scope.songs = [];
+
+    $scope.allChecked = false;
+    $scope.page = 0;
+
+    function detectAllChecked (checked) {
+        angular.forEach($scope.data.songs, function (song) {
+            song.checked = checked;
+        });
+    }
+
+    $scope.loadSongs = function () {
+
+        download.loadSongs(params.type, params.id)
+        .then(function (data) {
+            $scope.data = data;
+            $scope.title = data.title;
+
+            angular.forEach(data.songs, function (song, key) {
+                download.findItem(song.id)
+                .then(function (item) {
+                    download.updateStatus(song, item);
+                });
+            });
+            detectAllChecked($scope.allChecked);
+
+            $scope.status.error = false;
+            $scope.status.loaded = true;
+            if (!data.songs.length) {
+                $scope.status.ended = true;
+            }
+        }, function () {
+            $scope.status.error = true;
+        });
+    };
+    $scope.downloadSong = function (song) {
+        baidu.search({
+            title: song.title,
+            artist: song.artist,
+            album: song.album
+        })
+        .then(function (url) {
+            song.url = url;
+            chrome.downloads.download({
+                filename: song.title + ' - ' + song.artist + '.mp3',
+                url: url
+            }, function (downloadId) {
+                download.add(song, downloadId);
+                song.downloadId = downloadId;
+            });
+        }, function () {
+            song.error = true;
+        });
+    };
+    $scope.downloadSongs = function () {
+        $scope.downloadProcess = true;
+        async.eachSeries($scope.songs, function (song, callback) {
+            $scope.downloadSong(song);
+            $timeout(function () {
+                callback();
+            }, 3000);
+        }, function () {
+            $scope.downloadProcess = false;
+        });
+    };
+    $scope.pauseDownload = function (song) {
+        var downloadId = song.downloadId;
+        if (downloadId) {
+            chrome.downloads.pause(downloadId, function () {
+                song.isPaused = true;
+            });
+        }
+    };
+    $scope.resumeDownload = function (song) {
+        var downloadId = song.downloadId;
+        if (downloadId) {
+            chrome.downloads.resume(downloadId, function () {
+                song.isPaused = false;
+            });
+        }
+    };
+
+    chrome.downloads.onChanged.addListener(function (delta) {
+        download.findSong($scope.data.songs, delta.id)
+        .then(function (song) {
+            download.updateStatus(song, delta);
+        });
+    });
+
+    $scope.openUrlModal = function () {
+        $modal.open({
+            templateUrl: 'partails/song-url.html',
+            controller: 'SongUrlController',
+            resolve: {
+                songs: function () {
+                    var songs = angular.copy($scope.songs);
+                    songs = _.map(songs, function (song) {
+                        return {
+                            title: song.title,
+                            artist: song.artist,
+                            album: song.album
+                        };
+                    });
+                    return songs;
+                }
+            }
+        });
+    };
+    $scope.checkNeverDownload = function () {
+        angular.forEach($scope.data.songs, function (song) {
+            if (song.isCompleted) {
+                song.checked = false;
+            } else {
+                song.checked = true;
+            }
+        });
+    };
+
+    $scope.toggleChecked = function ($event, song) {
+        var target = $event.target;
+        if (target.nodeName === 'TD') {
+            song.checked = !song.checked;
+        }
+    };
+
+    $scope.$watch('data.songs', function (songs) {
+        $scope.songs = _.filter(songs, function (song) {
+            return song.checked;
+        });
+    }, true);
+    $scope.$watch('allChecked', function (checked) {
+        detectAllChecked(checked);
+    });
+
+    $scope.loadSongs();
+
+
+}]);
+
+angular
+.module('fmApp')
 .controller('SongUrlController', ['$scope', '$modalInstance', 'songs', '$timeout', 'copy', 'baidu',
     function ($scope, $modalInstance, songs, $timeout, copy, baidu) {
     "use strict";
@@ -4826,6 +4978,10 @@ angular
                     if ($items.length) {
                         songs = $.map($items, function (item) {
                             var $item = $(item),
+                                $albumInfo = $item.next('.detail-info'),
+                                $album = $albumInfo.find('.album-info a').first(),
+                                album = $.trim($album.find('.album-title').text()),
+                                albumUrl = $album.attr('href'),
                                 sid = $item.data('songid'),
                                 ssid = $item.data('ssid'),
                                 $info = $item.find('.song-info'),
@@ -4840,10 +4996,10 @@ angular
                                     id: sid,
                                     ssid: ssid,
                                     title: helper.fixFilename(title),
-                                    album: '',
+                                    album: album,
                                     fmUrl: helper.fmUrl(sid, ssid),
-                                    albumUrl: '',
-                                    albumId: '',
+                                    albumUrl: albumUrl,
+                                    albumId: albumUrl.replace(/.*\/(\d+)\/?$/, '$1'),
                                     artist: artist,
                                     artistUrl: artistUrl,
                                     programme: programme,
