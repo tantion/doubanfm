26e776ba038cb5a178bd8e0d00ec6272f1569614
tantion <hillcom@qq.com>
2014-04-05 05:52:18 +0800

    Fixed

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,4 +1,4 @@
-/*! douban-fm-improve - v2.1.1 - 2014-04-05
+/*! douban-fm-improve - v2.1.2 - 2014-04-05
 * https://github.com/tantion/doubanfm
 * Copyright (c) 2014 tantion; Licensed MIT */
 (function(global, undefined) {
