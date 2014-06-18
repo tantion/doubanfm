bcff55bbadd13839486564f99f28b07c3d639d28
tantion <hillcom@qq.com>
2014-06-18 23:07:30 +0800

    package v2.2.2

--- src/angular-seajs.js
+++ src/angular-seajs.js
@@ -1,4 +1,4 @@
-/*! douban-fm-improve - v2.2.1 - 2014-06-18
+/*! douban-fm-improve - v2.2.2 - 2014-06-18
 * https://github.com/tantion/doubanfm
 * Copyright (c) 2014 tantion; Licensed MIT */
 (function(global, undefined) {
